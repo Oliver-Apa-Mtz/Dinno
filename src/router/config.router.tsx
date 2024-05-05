85a86f1b6e315c9565071297e780377c0ae4fb3e
Oliver Aparicio Martinez <olivermtz35@gmail.com>
2024-05-04 19:09:50 -0600

    FTR: probando qa

--- src/router/config.router.tsx
+++ src/router/config.router.tsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 
 import { ScrollToTop } from '../utils/utils';
-/*import Home from "../pages/Home";
+import Home from "../pages/Home";
 import Features from "../pages/Features";
 import Prices from '../pages/Prices';
 import Contact from '../pages/Contact';
 import Blog from '../pages/Blog';
-import ThankYou from '../pages/Thanks';*/
+import ThankYou from '../pages/Thanks';
 
 import { Analytics } from "@vercel/analytics/react"
-import Building from "../pages/Building";
+//import Building from "../pages/Building";
 
 export const Router = () => {
 	return (
@@ -17,7 +17,7 @@ export const Router = () => {
 			<ScrollToTop />
 			<Analytics />
 			<Routes>
-				{/*<Route path="/" element={<Home />} />
+				<Route path="/" element={<Home />} />
 				<Route path="/inicio" element={<Home />} />
 				<Route path="/caracteristicas" element={<Features />} />
 				<Route path="/precios" element={<Prices />} />
@@ -25,8 +25,8 @@ export const Router = () => {
 				<Route path="/contacto/:paquete" element={<Contact />} />
 				<Route path="/blog" element={<Blog />} />
 				<Route path="/nosotros" element={<Blog />} />
-	<Route path="/gracias-por-contactarnos" element={<ThankYou />} />*/}
-				<Route path="/" element={<Building />} />
+				<Route path="/gracias-por-contactarnos" element={<ThankYou />} />
+				{/*<Route path="/" element={<Building />} />*/}
 			</Routes>
 		</>
 	);
